66ced1a15e9d5382efd93c3a4f372f1e9685bdcb
Brian Levin <brian4lawschool@gmail.com>
2020-05-14 11:24:10 +0200

    Daily push for 20200514

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # COVID-STATS
-Kosovo COVID Stats
+
+This is Open Source data about COVID-19 cases in Kosovo. The scripts used in this program should be run as follows:
+`ipython covid.py`
+
+***This code was writen on a mac. It will require modification to work on other operating systems.***
--- tasks.py
+++ tasks.py
@@ -1,5 +1,19 @@
 from invoke import task
+import datetime
+
+@task
+def convertnb(c, docs=False):
+    c.run('jupyter nbconvert --to script covid.ipynb')
+
+def convertpy(c, docs=False):
+    c.run('ipynb-py-convert covid.py test.ipynb')
 
 @task
 def build(c, docs=False):
-    c.run("jupyter nbconvert --to script covid.ipynb")
+    c.run('ipython covid.py')
+
+@task
+def push(c, docs=False):
+    c.run('git add .')
+    commit_message = '\"Daily push for {:%Y%m%d}\"'.format(datetime.date.today())
+    c.run('git commit -m {0}'.format(commit_message))
