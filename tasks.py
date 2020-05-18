7246c54d79c5065894fcfda5a42274fac5072ad9
Brian Levin <brian4lawschool@gmail.com>
2020-05-18 15:11:40 +0200

    Daily push for 20200518

--- tasks.py
+++ tasks.py
@@ -15,12 +15,15 @@ def convertpy(c, docs=False):
 @task
 def build(c, docs=False):
     c.run('ipython covid.py')
-    c.run('zip -j ./Releases/{0}.zip ./Graphics/{0}/*'.format(foldername))
 
 @task
 def push(c, docs=False):
     c.run('git add --all')
     c.run('git commit -am {0}'.format(commit_message))
-    c.run('git tag {0}'.format(foldername))
     c.run('git push origin master')
+
+@task
+def release(c, docs=False):
+    c.run('zip -j ./Releases/{0}.zip ./Graphics/{0}/*'.format(foldername))
+    c.run('git tag {0}'.format(foldername))
     c.run('hub release create -o -a ./Releases/{0}.zip -m \"Covid Graphs for {0}.\" {0}'.format(foldername))
